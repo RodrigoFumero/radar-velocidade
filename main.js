30fe3fa6d941fff7768205d89e1fd4e6e7092584
RodrigoFumero <63894087+RodrigoFumero@users.noreply.github.com>
2022-03-29 15:06:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let soma = 0
+          
+
+    function verificar(velocidade) {
+        let elementoVelocidade = document.getElementById("txtVelocidade").value
+    pontos = Math.floor((elementoVelocidade - 70) / 5)
+        if (pontos < 0) {
+            if(soma >= 0 && soma < 1) { resultado.innerHTML = "Motorista habilitado."
+            } else if(soma > 0 && soma < 21) {
+            resultado.innerHTML = `Multa de ${pontos} pontos, habilitação com ${soma} pontos`
+            } else { resultado.innerHTML = `Carteira suspensa ${pontos}, motorista com ${soma} na habilitação.`
+            }
+        } else {
+            soma += pontos
+            if(soma >= 0 && soma < 1) { resultado.innerHTML = "Motorista habilitado."
+            } else if(soma > 0 && soma < 21) {
+            resultado.innerHTML = `Multa de ${pontos} pontos, habilitação com ${soma} pontos.`
+            } else { resultado.innerHTML = `Multa de ${pontos} pontos, HABILITAÇÃO SUSPENSA, motorista com ${soma} na habilitação.`
+            }
+        }
+    }
